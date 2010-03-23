e375a6aed413197b39f92b45589bbb9285477752
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-03-23 13:35:04 +0000

    ah

---
+++ xforms/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=SunStudio-Solaris-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/SunStudio-Solaris-x86
+CND_ARTIFACT_NAME_Debug=xforms
+CND_ARTIFACT_PATH_Debug=dist/Debug/SunStudio-Solaris-x86/xforms
+CND_PACKAGE_DIR_Debug=dist/Debug/SunStudio-Solaris-x86/package
+CND_PACKAGE_NAME_Debug=xforms.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/SunStudio-Solaris-x86/package/xforms.tar
+# Release configuration
+CND_PLATFORM_Release=SunStudio-Solaris-x86
+CND_ARTIFACT_DIR_Release=dist/Release/SunStudio-Solaris-x86
+CND_ARTIFACT_NAME_Release=xforms
+CND_ARTIFACT_PATH_Release=dist/Release/SunStudio-Solaris-x86/xforms
+CND_PACKAGE_DIR_Release=dist/Release/SunStudio-Solaris-x86/package
+CND_PACKAGE_NAME_Release=xforms.tar
+CND_PACKAGE_PATH_Release=dist/Release/SunStudio-Solaris-x86/package/xforms.tar
