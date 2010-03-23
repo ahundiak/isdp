3bb8d5561c4a6c369b78a4f3d5b38924714c6415
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-03-23 17:40:01 +0000

    ah

---
+++ build/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=SunStudio-Solaris-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/SunStudio-Solaris-x86
+CND_ARTIFACT_NAME_Debug=build
+CND_ARTIFACT_PATH_Debug=dist/Debug/SunStudio-Solaris-x86/build
+CND_PACKAGE_DIR_Debug=dist/Debug/SunStudio-Solaris-x86/package
+CND_PACKAGE_NAME_Debug=build.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/SunStudio-Solaris-x86/package/build.tar
+# Release configuration
+CND_PLATFORM_Release=SunStudio-Solaris-x86
+CND_ARTIFACT_DIR_Release=dist/Release/SunStudio-Solaris-x86
+CND_ARTIFACT_NAME_Release=build
+CND_ARTIFACT_PATH_Release=dist/Release/SunStudio-Solaris-x86/build
+CND_PACKAGE_DIR_Release=dist/Release/SunStudio-Solaris-x86/package
+CND_PACKAGE_NAME_Release=build.tar
+CND_PACKAGE_PATH_Release=dist/Release/SunStudio-Solaris-x86/package/build.tar
