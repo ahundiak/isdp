683f99782527d7492bf044b30fa9d4b291a341fa
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-03-28 16:24:54 +0000

    ah

--- build/build/build.sh
+++ build/build/build.sh
@@ -5,19 +5,20 @@
 #
 # Created on March 24, 2010, 3:21 PM
 #
-# Installs the deltools products.
+# Installs the various ISDP products
 
-export ISDP_SOURCE=$HOME/isdp
-export ISDP_INSTALL=$HOME/ingr
-export ISDP_LEGACY=$HOME/save/ingr
-
-mkdir -p $ISDP_INSTALL/bin
+# These need to be set in the env file
+# export ISDP_SOURCE=$HOME/isdp
+# export ISDP_INSTALL=$HOME/ingr
+# export ISDP_LEGACY=$HOME/save/ingr
 
 # File containg info about each installed product
+mkdir -p $ISDP_INSTALL
 cp -p -f $ISDP_SOURCE/build/install/ingrconfig $ISDP_INSTALL
 
 # proddata uses ingrconfig
 # cp -p $ISDP_LEGACY/deltools/proddata $ISDP_INSTALL/bin
 #chmod +x $ISDP_INSTALL/bin/proddata
-cd $ISDP_SOURCE/deltools/src/dates; make install; cd -
+cd $ISDP_SOURCE/deltools; make -e install; cd
 
+# UMS Message compiler and library
