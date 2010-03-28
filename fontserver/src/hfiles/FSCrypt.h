dd09678ce38713790ee323bebbafdcec66216d6c
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-03-28 13:58:11 +0000

    ah

---
+++ fontserver/src/hfiles/FSCrypt.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   FSCrypt.h
+ * Author: impd
+ *
+ * Created on March 24, 2010, 5:07 PM
+ */
+
+#ifndef _FSCRYPT_H
+#define	_FSCRYPT_H
+
+#include "../hfiles/FSTypes.h"
+
+extern  int     _FSDecryptBuff (Int16 *buff, int numWords);
+
+#endif	/* _FSCRYPT_H */
+
---
+++ fontserver/src/hfiles/FSScanConv.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   FSScanConv.h
+ * Author: impd
+ *
+ * Created on March 25, 2010, 12:03 PM
+ */
+
+#ifndef _FSSCANCONV_H
+#define	_FSSCANCONV_H
+
+#include "../hfiles/FSDef.h"
+#include "../hfiles/FSPolyFill.h"
+
+extern  Int     _FSGetCompToken (ArcType *coord);
+
+#endif	/* _FSSCANCONV_H */
