e182ee183840ac2cbfae987e240801c1cdbb4ca3
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-04-13 20:29:11 +0000

    Initial version that mostly builds correctly.  Grnuc is still not being built, and some ppl compiles are failing.

---
+++ trunk/build/bin/version.c
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdio.h>
+
+int find_decimals (char *str, int no)
+  {
+    char i_str[81];
+    char str1[81], str2[81], str3[81]; 
+    int  i = 0, j =0, k = 0;
+    int  value;
+   
+    sscanf (str, "%80s", i_str);
+    while (i <= no)
+      {
+        if (i_str[i] == '.')
+         {
+           k++; 
+           j = 0;
+         }
+        else
+         {
+           if (k == 0) 
+             str1[j] = i_str[i];
+           if (k == 1)
+             str2[j] = i_str[i];
+           if (k == 2)
+             str3[j] = i_str[i];
+           j++;
+         }
+         i++;
+      }
+    if (k >= 0 ) 
+      sscanf (str1, "%d", &value);
+    if (k >= 1) 
+      {
+        i = value;
+        sscanf (str2, "%d", &j);
+        value = j + (i * 256);
+      }
+    if (k >= 2)
+      {
+        sscanf (str3, "%d", &value);  
+        value = value + (j * 256) + (i * 4096);
+      }
+    return (value);
+  }
+
+int convert_version (char *str)
+  {
+    int   len_str, out_val;
+   
+    len_str = strlen (str);
+    out_val = find_decimals (str, len_str);
+    return (out_val);
+  }
