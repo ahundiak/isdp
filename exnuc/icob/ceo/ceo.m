26f4ba980ad08237a334c1581674ced3ad997671
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-10-28 19:18:46 +0000

    ah

--- exnuc/icob/ceo/ceo.m
+++ exnuc/icob/ceo/ceo.m
@@ -1,23 +1,3 @@
-SRC
-$(EXNUC)/icob/ceo
-
-INCLUDE
-$(EXNUC)/include
-$(UMS_INC_PATH)
-#if defined (ENV5)
-$(FORMS_INC_PATH)
-#elif defined (X11)
-$(XFORMSDP_INC_PATH)
-$(X11_INC_PATH)
-#endif
-#if defined (XGL)
-$(XGLHOME)/include
-$(XGLHOME)/include/xgl
-#endif
-
-SPEC
-$(EXNUC)/spec
-
 SOURCE
 action.I
 arg_button.I
@@ -40,17 +20,19 @@ status.I
 status_disp.I
 wakeup.I
 
-#if defined(CLIX) || defined(SUNOS) || defined(IRIX)
 LIB
-$(TARGET)/icob/lib/ceo.o
-#elif defined (NT)
-ARCHIVE
-$(TARGET)/icob/lib/ceo.lib
-#endif
+$EXNUC/lib/icob_ceo.o
+
+INCLUDE
+$UMS/include
+$FORMS/include
+$EXNUC/include
+
+SPEC
+$EXNUC/spec
 
 OPP
-$(BASE)/bin/opp
+$EXNUC/bin/opp
 
 OMCPP
-$(BASE)/bin/omcpp
-
+$EXNUC/bin/omcpp
