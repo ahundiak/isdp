ce7308d946dadfb56a50c046e103fb7868f5553a
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-10-28 19:31:17 +0000

    ah

--- trunk/exnuc/icob/trans/trans.m
+++ trunk/exnuc/icob/trans/trans.m
@@ -1,35 +1,21 @@
-SRC
-$(EXNUC)/icob/trans
-
-INCLUDE
-$(EXNUC)/include
-$(UMS_INC_PATH)
-#if defined (X11)
-$(X11_INC_PATH)
-#endif
-#if defined (XGL)
-$(XGLHOME)/include
-$(XGLHOME)/include/xgl
-#endif
-
-SPEC
-$(EXNUC)/spec
-
 SOURCE
 do_it_t.I
 set_t.I
 
-#if defined(CLIX) || defined(SUNOS) || defined(IRIX)
 LIB
-$(TARGET)/icob/lib/trans.o
-#elif defined (NT)
-ARCHIVE
-$(TARGET)/icob/lib/trans.lib
-#endif
+$EXNUC/lib/icob_trans.o
+
+INCLUDE
+$UMS/include
+$FORMS/include
+$EXNUC/include
+
+SPEC
+$EXNUC/spec
 
 OPP
-$(BASE)/bin/opp
+$EXNUC/bin/opp
 
 OMCPP
-$(BASE)/bin/omcpp
+$EXNUC/bin/omcpp
 
