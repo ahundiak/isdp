282484319fbb0a9acfb7b9147b77bde1b59dc2e0
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-10-28 18:57:24 +0000

    ah

---
+++ exnuc/om/testx/OMeveryone.h
@@ -0,0 +1,16 @@
+#ifndef OMeveryone_include
+#define OMeveryone_include
+
+#ifndef   _STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef	  OM_D_PRIMITIVES
+#include "OMprimitives.h"  /* Gets OMminimum,OMlimits,OMtypes */
+#endif
+
+#ifndef	  OM_D_ERRORDEF
+#include "OMerrordef.h"
+#endif
+
+#endif
