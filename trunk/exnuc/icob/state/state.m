eb6373b11291796e4936bb69a60f5cb5d8fcbeb6
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-10-28 19:25:34 +0000

    ah

--- trunk/exnuc/icob/state/state.m
+++ trunk/exnuc/icob/state/state.m
@@ -1,34 +1,19 @@
-SRC
-$(EXNUC)/icob/state
-
-INCLUDE
-$(EXNUC)/include
-$(UMS_INC_PATH)
-#if defined (X11)
-$(X11_INC_PATH)
-#endif
-#if defined (XGL)
-$(XGLHOME)/include
-$(XGLHOME)/include/xgl
-#endif
-
-SPEC
-$(EXNUC)/spec
-
 SOURCE
 do_it_s.I
 
-#if defined(CLIX) || defined(SUNOS) || defined(IRIX)
 LIB
-$(TARGET)/icob/lib/state.o
-#elif defined (NT)
-ARCHIVE
-$(TARGET)/icob/lib/state.lib
-#endif
+$EXNUC/lib/icob_state.o
+
+INCLUDE
+$UMS/include
+$FORMS/include
+$EXNUC/include
+
+SPEC
+$EXNUC/spec
 
 OPP
-$(BASE)/bin/opp
+$EXNUC/bin/opp
 
 OMCPP
-$(BASE)/bin/omcpp
-
+$EXNUC/bin/omcpp
