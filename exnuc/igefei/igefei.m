5e8416cf567d42acaea8f3b836f29ee3d8223f9e
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-10-29 15:18:59 +0000

    ah

--- exnuc/igefei/igefei.m
+++ exnuc/igefei/igefei.m
@@ -1,32 +1,19 @@
-SRC
-$(EXNUC)/igefei
-
-INCLUDE
-$(EXNUC)/include
-$(FONTSERVE_INC_PATH)
-#if defined (ENV5)
-$(FORMS_INC_PATH)
-#elif defined (X11)
-$(XFORMSDP_INC_PATH)
-$(X11_INC_PATH)
-#endif
-#if defined (XGL)
-$(XGLHOME)/include
-$(XGLHOME)/include/xgl
-#endif
-
-SPEC
-$(EXNUC)/spec
-
 SOURCE
 igefei.I
 
 LIB
-$(TARGET)/lib/igefei.o
+$EXNUC/lib/igefei.o
+
+SPEC
+$EXNUC/spec
+
+INCLUDE
+$FORMS/include
+$FORMS/fs_include
+$EXNUC/include
 
 OPP
-$(BASE)/bin/opp
+$EXNUC/bin/opp
 
 OMCPP
-$(BASE)/bin/omcpp
-
+$EXNUC/bin/omcpp
