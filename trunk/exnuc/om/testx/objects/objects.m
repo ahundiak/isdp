31d60c1a0e3709db6f1ee40b28e8bee4f203e2c7
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-11-04 17:39:44 +0000

    ah

---
+++ trunk/exnuc/om/testx/objects/objects.m
@@ -0,0 +1,32 @@
+SOURCE
+A.S      Ai.I
+B.S      Bi.I
+C.S      Ci.I
+D.S      Di.I
+ResA.S   ResAi.I
+ResB.S   ResBi.I
+classd.S classdi.I
+class1.S class1i.I
+class2.S
+class3.S
+class4.S
+class5.S
+parenta.S parentai.I
+parentb.S parentbi.I
+parentc.S parentci.I
+SendTime.S SendTime2.S SendTimei.I
+
+LIB
+objects.o
+
+SPEC
+$EXNUC/spec
+
+INCLUDE
+$EXNUC/include
+
+OPP
+$EXNUC/bin/opp
+
+OMCPP
+$EXNUC/bin/omcpp
