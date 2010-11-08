f0378424e27bb8b101dd857d692fc6c74e3765af
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-11-08 15:26:02 +0000

    ah

--- trunk/model/hsurf/src/driver_func/spec.m
+++ trunk/model/hsurf/src/driver_func/spec.m
@@ -1,23 +1,18 @@
 SRC
 $(MODEL)/hsurf/src/driver_func
+
 LIB
-$(MDTARG)/hsurf/src/driver_func/driver_func.o
-#if defined (sun)
-CC
-cc
-#else
-CC
-acc -knr
-#endif
-COPT
--ga
+$(MDTARG)/src_lib/hsurf_driver_func.o
+
 SPEC
 $(MODEL)/hsurf/src/spec
 $(EXNUC)/spec
 $(GRNUC)/spec
 $(MODEL)/spec
+
 INCLUDE
-$(INGRHOME)/include
+$(UMS)/include
+$(FORMS)/include
 $(GRNUC)/ingrsys
 $(MODEL)/hsurf/src/include
 $(EXNUC)/include
@@ -27,17 +22,13 @@ $(MODEL)/proto_include
 $(BS)/include
 $(BS)/include/prototypes
 $(BS)/include/prototypes
-#ifdef X11
-$(XINC)
-#endif
-#if defined (XGL)
-$(XGLHOME)/include
-$(XGLHOME)/include/xgl
-#endif
+
 OPP
 $(EXTARG)/bin/opp
+
 OMCPP
 $(EXTARG)/bin/omcpp
+
 SOURCE
 HSgetedges.I
 HStildriver.C
