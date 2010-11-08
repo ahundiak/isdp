18207d051ef78ebd48824723950f85867b404e2e
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-11-08 22:21:11 +0000

    ah

---
+++ trunk/xforms/source/xfi/src10/FIconfbox.c
@@ -0,0 +1,4 @@
+/* ==========================================
+ * Stub function to keel Model happy
+ */
+FI_confirm_box                     (){return(0);}
---
+++ trunk/xforms/source/xfi/src10/FIfsetorig.c
@@ -0,0 +1,4 @@
+/* ==========================================
+ * Stub function to keel Model happy
+ */
+FIf_set_orig_coord                 (){return(0);}
