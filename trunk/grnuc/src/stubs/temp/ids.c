553ebac1b8461f6f6c7b97cbbdc5242e6524b035
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-11-10 15:27:18 +0000

    ah

---
+++ trunk/grnuc/src/stubs/temp/ids.c
@@ -0,0 +1,11 @@
+typedef unsigned short int OMuword;
+
+OMuword OPP_EMScnstr_class_id     = 65535;
+OMuword OPP_SKgmhndlpt_class_id   = 65535;
+OMuword OPP_COcctform_class_id    = 65535;
+OMuword OPP_EMSdimdtm_class_id    = 65535;
+OMuword OPP_ECdmcnstr_class_id    = 65535;
+OMuword OPP_EMSdatpln_class_id    = 65535;
+OMuword OPP_SKconstraint_class_id = 65535;
+OMuword OPP_EMSidentity_class_id  = 65535;
+OMuword OPP_EMSpointer_class_id   = 65535;
