9064f71234644f91c589c612a23287848af213c9
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-11-11 13:55:40 +0000

    ah

--- trunk/vds/vdris/imp/VdsRisLogin.C
+++ trunk/vds/vdris/imp/VdsRisLogin.C
@@ -13,33 +13,6 @@
  *	$Log: VdsRisLogin.C,v $
  *	Revision 1.1.1.1  2001/01/04 21:09:12  cvs
  *	Initial import to CVS
- *	
- * Revision 1.1  1998/04/29  16:27:20  pinnacle
- * VDS 2.5.1
- *
- * Revision 1.1  1997/05/08  13:20:44  pinnacle
- * New project VDS 250
- *
- * Revision 1.1  1996/07/19  20:33:52  pinnacle
- * Create VDS 241 project
- *
- * Revision 1.5  1996/04/21  23:27:24  pinnacle
- * Replaced: vdris/imp/*.C for:  by v240_int for vds.240
- *
- * Revision 1.4  1995/12/20  19:46:44  pinnacle
- * Replaced: vdris/imp/VdsRisLogin.C for:  by jwfrosch for vds.240
- *
- * Revision 1.3  1995/11/29  13:53:52  pinnacle
- * Replaced: ./vdris/imp/VdsRisLogin.C for:  by ksundar for vds.240
- *
- * Revision 1.2  1995/09/25  23:10:14  pinnacle
- * Replaced: vdris/imp/VdsRisLogin.C for:  by sundar for vds.240
- *
- * Revision 1.1  1994/11/14  22:42:32  pinnacle
- * Create vds.240 from vds.232
- *
- * Revision 1.1  1994/11/08  16:07:00  pinnacle
- * create vds.232
  *
  *
  * History:
@@ -169,7 +142,7 @@ static int VDris_Get_text ( fp, g_label, text, text_size )
 int	VdsLoginNotif(f_label, g_label, value, fp)
 int	f_label;
 int	g_label;
-double	value;
+IGRdouble	value;
 Form	fp;
 {
    static char	environ_name[20];
