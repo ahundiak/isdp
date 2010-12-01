73fbc121b9b5ceafdd25952c7765e866f0236a08
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-12-01 22:07:55 +0000

    ah

---
+++ trunk/ris/src/com/arch/arcglobs.c
@@ -0,0 +1,11 @@
+/* Do not alter this SPC information: $Revision: 1.1.55.1 $ */
+/*
+**	NAME:							arcglobs.c
+**	AUTHORS:						Terry McIntyre
+**	CREATION DATE:					11/91
+**	ABSTRACT:
+**	
+**	REVISION HISTORY:
+*/
+#define ARC_DEFINE
+#include "risarch.h"
---
+++ trunk/ris/src/com/arch/imp.m
@@ -0,0 +1,10 @@
+SOURCE
+arcerrcd.c
+arcgnerc.c
+arcglobs.c
+
+LIB
+$RIS/lib/com_arch.o
+
+INCLUDE
+$RIS/include
