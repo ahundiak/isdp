b1846c4f64ea52d55144e5cdebd62f35e33132e4
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-12-04 15:33:13 +0000

    ah

---
+++ auto/exec/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: impd
+ *
+ * Created on December 4, 2010, 8:20 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    printf("Hello from %s\n",argv[0]);
+    return (EXIT_SUCCESS);
+}
+
