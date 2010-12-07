d205b4b8ffdae7072249a504c14984085b33f0e4
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-12-07 20:01:42 +0000

    ah

--- vds/vdaim/sir2/imp.m
+++ vds/vdaim/sir2/imp.m
@@ -13,35 +13,6 @@
  *      $Log: AimSir2.m,v $
  *      Revision 1.2.2.7  2004/03/29 17:26:53  ahundiak
  *      ah
- *
- *      Revision 1.2.2.6  2004/02/27 15:00:07  ahundiak
- *      ah
- *
- *      Revision 1.2.2.5  2003/09/10 15:56:22  ahundiak
- *      ah
- *
- *      Revision 1.2.2.4  2003/06/11 13:31:19  ahundiak
- *      ah
- *
- *      Revision 1.2.2.3  2003/06/06 20:44:19  ahundiak
- *      ah
- *
- *      Revision 1.2.2.2  2003/06/04 18:13:42  ahundiak
- *      ah
- *
- *      Revision 1.2.2.1  2003/05/30 19:08:54  ahundiak
- *      ah
- *
- *      Revision 1.2  2003/05/27 13:35:15  ahundiak
- *      ah
- *
- *      Revision 1.1  2003/05/16 14:39:34  ahundiak
- *      ah
- *
- *      Revision 1.5.2.1  2003/04/08 16:18:56  ahundiak
- *      ah
- *
- *
  * History:
  * 03/04/08  ah  Added Header, Standard SP Format
  * -------------------------------------------------------------------*/
@@ -52,7 +23,6 @@ VDPaimDelete.I
 VDPaimPostMan.c
 VDPaimPost.c
 VDPaimPost2.c
-VDPaimUtil.c
 VDPaimValidate.c
 VDPaimOccTpl.c
 VDPaimGroup.c
@@ -63,11 +33,7 @@ VDcoll.I
 VDlogExt.c
 
 LIB
-#if ISDP_MAKE_SP
-$SPXA/src/lib/AimSir2.o
-#else
-$AIM/src/lib/AimSir2.o
-#endif
+$VDS/lib/vdiaimsir2.o
 
 SPEC
 $EXNUC/spec
@@ -77,25 +43,16 @@ $EMS/spec
 $VDS/spec
 
 INCLUDE
-#if ISDP_MAKE_SP
-$SPXA/include
-$SPXV/include
-$SPXV/vdinclude
-#endif
-$LOCAL/include
 $BS/include
 $UMS/include
 $PDU/include
 $PDUDP/include
 $NFM/include
-$NFMDP/include
 $FORMS/include
 $EXNUC/include
 $GRNUC/include
-$GRNUC/ingrsys
 $GRNUC/src/assoc/include_priv
 $MODEL/include
-$AIM/include
 $VDS/include
 $VDS/include/prototypes
 $VDS/vdinclude
