abf180b355f831aed8cbe22d5bd65a2aaf782f0d
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-12-14 00:04:26 +0000

    ah

--- auto/base/ris/main.c
+++ auto/base/ris/main.c
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include <glib/gstdio.h>
+#include <glib-object.h>
 
 #include "VDrisc.h"
 
@@ -96,10 +97,63 @@ void test_query_storage_areas()
 */
   VDrisFreeInfo(&info);
 }
+void test_nothing()
+{
+  // Set this to false generates core file
+  g_assert(TRUE);
+}
+/* ============================================================================
+ */
+extern int ris_net_connect();
+extern int ris_net_disconnect();
+extern int ris_net_get_ris_server_path(char *path);
+extern int ris_net_check_arch();
+extern int ris_net_get_table_info();
+extern int ris_net_open_schema();
+
+void test_ris_net_connect()
+{
+  int sts;
+  char path[256];
+
+  sts = ris_net_connect();
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+
+  sts = ris_net_get_ris_server_path(path);
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+  g_assert_cmpstr(path,==,"D:\\RIS\\RIS06.00");
+  
+  sts = ris_net_disconnect();
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+
+}
+void test_ris_net_init()
+{
+  int sts;
+
+  sts = ris_net_connect();
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+
+  sts = ris_net_check_arch();
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+
+  sts = ris_net_open_schema();
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+
+  sts = ris_net_get_table_info();
+  g_assert_cmpint(RIS_SUCCESS, ==, sts);
+
+}
 int main(int argc, char** argv)
 {
+  g_type_init();
   g_test_init (&argc, &argv, NULL);
 
+  g_test_add_func("/base/ris nothing", test_nothing);
+  g_test_add_func("/base/ris init",    test_ris_net_init);
+//g_test_add_func("/base/ris connect", test_ris_net_connect);
+
+#if 0
   g_test_add_func("/base/ris open_schema", test_open_schema);
   
   g_test_add_func("/base/ris  get_schema", test_get_schema);
@@ -107,7 +161,7 @@ int main(int argc, char** argv)
   g_test_add_func("/base/ris  query sas ", test_query_storage_areas);
 
   g_test_add_func("/base/ris  query part", test_query_part);
-
+#endif
   return g_test_run();
 }
 
