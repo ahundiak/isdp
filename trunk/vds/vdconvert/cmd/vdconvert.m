2fa343f007b27f6485435cc8d3e7f758e4aa518b
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2010-12-21 14:11:21 +0000

    ah

--- trunk/vds/vdconvert/cmd/vdconvert.m
+++ trunk/vds/vdconvert/cmd/vdconvert.m
@@ -14,33 +14,6 @@
  *	Revision 1.1.1.1  2001/01/04 21:07:58  cvs
  *	Initial import to CVS
  *	
-# Revision 1.1  1998/04/29  16:27:20  pinnacle
-# VDS 2.5.1
-#
-# Revision 1.1  1997/05/08  13:20:44  pinnacle
-# New project VDS 250
-#
-# Revision 1.1  1996/07/19  20:33:52  pinnacle
-# Create VDS 241 project
-#
-# Revision 1.5  1995/09/26  14:44:26  pinnacle
-# Replaced: vdconvert/cmd/vdconvert.m for:  by sundar for vds.240
-#
-# Revision 1.4  1995/07/31  17:24:10  pinnacle
-# Replaced: vdconvert/cmd for:  by hverstee for vds.240
-#
-# Revision 1.3  1995/07/30  19:24:24  pinnacle
-# Replaced: vdconvert/cmd/vdconvert.m for:  by hverstee for vds.240
-#
-# Revision 1.2  1995/07/24  16:43:16  pinnacle
-# Replaced: vdconvert/cmd/vdconvert.m for:  by hverstee for vds.240
-#
-# Revision 1.1  1994/11/14  22:42:32  pinnacle
-# Create vds.240 from vds.232
-#
-# Revision 1.1  1994/11/08  16:07:00  pinnacle
-# create vds.232
-#
  *
  * History:
  *	MM/DD/YY	AUTHOR		DESCRIPTION
