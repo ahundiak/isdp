9564c30218dde8285270ab2e34dac6360740818a
ahundiak <ahundiak@e9faf64c-7e38-11de-a453-d5a50d962d30>
2011-07-05 20:46:36 +0000

    ah

---
+++ trunk/auto/base/debug/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[])
+{
+  char *buf,*p;
+  FILE *file;
+
+  printf("Entering %s\n",argv[0]);
+  
+  // file = fopen("main.c","rt");
+
+  // fread(buf,250,1,file);
+
+  p = strchr(buf,'i');
+  
+  // fclose(file);
+}
+
